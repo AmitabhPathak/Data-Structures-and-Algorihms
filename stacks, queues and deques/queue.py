a0bb25c3e6f616fb73ecdb00b513dac0549e14f1
amitabh kumar <amitabh.kumar@nokia.com>
2020-05-10 21:34:15 +0530

    comminting program of queues

---
+++ stacks, queues and deques/queue.py
@@ -0,0 +1,14 @@
+class Queue:
+    def __init__(self):
+        self.items=[]
+    def enque(self,item):
+        self.items.append(item)
+    def deque(self):
+        if(len(self.items)!=0):
+           l=self.items[0]
+           self.items.remove(l)
+           return l
+    def isEmpty(self):
+        return self.items==[]
+    def size(self):
+        return len(self.items)
