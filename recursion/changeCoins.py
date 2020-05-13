3de8245b72ecf19209eea9e9c66b959aeb2e3e68
amitabh kumar <amitabh.kumar@nokia.com>
2020-05-14 01:48:05 +0530

    comminting program of fibonaci ad changecoins

--- recursion/changeCoins.py
+++ recursion/changeCoins.py
@@ -1,11 +1,11 @@
 def change(target,coins):
+    mx=target
     if(target==0):
         return 0
     for coin in coins:
-        mx=0
         if(coin<=target):
-            mx=max(mx,1+change(target-coin,coins))
+            mx=min(mx,1+change(target-coin,coins))
             #return mx
     return mx
-c=change(10,[1,5,10])
+c=change(10,[1,5])
 print(c)
